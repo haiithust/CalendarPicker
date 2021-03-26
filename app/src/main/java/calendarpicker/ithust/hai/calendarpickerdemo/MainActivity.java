c84cf8c33697e41464b4ec1483a1020d24c23a57
CongHai <conghai.luong@foodycorp.vn>
2021-03-26 13:16:53 +0700

    [Calendar] migrate to android X

--- app/src/main/java/calendarpicker/ithust/hai/calendarpickerdemo/MainActivity.java
+++ app/src/main/java/calendarpicker/ithust/hai/calendarpickerdemo/MainActivity.java
@@ -4,10 +4,11 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Arrays;
--- calendarpicker/src/main/java/ithust/hai/calendarpicker/CalendarPickerView.java
+++ calendarpicker/src/main/java/ithust/hai/calendarpicker/CalendarPickerView.java
@@ -5,15 +5,16 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.text.DateFormat;
 import java.text.DateFormatSymbols;
